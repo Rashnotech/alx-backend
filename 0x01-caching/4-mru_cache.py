3c63714939ee031247ebd9f6906f465d0f6e1b61
Rashnotech <rashnotech@gmail.com>
2024-02-01 01:33:40 +0000

    Fixed: most recently used

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """a module that implement caching system"""
 from base_caching import BaseCaching
-
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
@@ -12,17 +12,21 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """class initialization"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """a method that assign to cache"""
         if not key or not item:
             return None
-        self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print('DISCARD: {}'.format())
+            last_key, _ = self.cache_data.popitem(False)
+            print('DISCARD: {}'.format(last_key))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, False)
 
     def get(self, key):
         """ a method that get value using key"""
         if not key or key not in self.cache_data.keys():
             return None
+        self.cache_data.move_to_end(key, last=False)
         return self.cache_data[key]
