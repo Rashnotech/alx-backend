d08164df00c605b560077a8e3b8106fccf8c5416
Rashnotech <rashnotech@gmail.com>
2024-02-07 04:42:45 +0000

    Force locdale with url parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,13 +19,16 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """get localization"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """render html template"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
