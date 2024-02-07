9ac2eccddd3e8083184b6e2a2e9c8e0574269c71
Rashnotech <rashnotech@gmail.com>
2024-02-07 04:33:47 +0000

    Fixed: use parameters in templates

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""flask app entry point"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """app localization"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get localization"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """render html template"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
