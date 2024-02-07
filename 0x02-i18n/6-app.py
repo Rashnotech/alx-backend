e23e565e1007b01cc20615024f04aa7f7ec561d1
Rashnotech <rashnotech@gmail.com>
2024-02-07 21:34:54 +0000

    Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -57,7 +57,7 @@ def before_request() -> None:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """render html template"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
